e3718f4936047c8b56f90226aa32b50db20d9fe0
Belicio Batista Cardoso <belloinfo@gmail.com>
2023-01-19 06:25:33 -0300

    Route created from details of habits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -30,6 +30,46 @@ export async function appRoutes(app: FastifyInstance) {
 
     });
 
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+
+        // const parseDate = dayjs(date).startOf('day')
+        const weekDay = dayjs(date).get('day')
+
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: dayjs(date).startOf('day').toDate()
+            },
+            include: {
+                dayHabits: true
+            }
+        })
+
+        const completHabits = day?.dayHabits.map(dayHabit => dayHabit.habit_id)
+
+        return {
+            possibleHabits, completHabits
+        }
+    })
+
     // app.get('/listhabits', async ()=>{
     //     const habits = await prisma.habit.findMany({
     //         where: {
