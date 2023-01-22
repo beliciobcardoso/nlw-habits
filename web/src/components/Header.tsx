e705929361295b3f0a5a73210dc2d535ac52a09c
Belicio Batista Cardoso <belloinfo@gmail.com>
2023-01-22 04:44:28 -0300

    Created modal Create Habits

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,14 +1,29 @@
 import logoImage from '../assets/logo.svg';
-import { Plus } from 'phosphor-react';
+import { Plus, X } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewHabitForm } from './NewHabitForm';
 
 export function Header() {
   return (
     <div className='Header'>
       <img src={logoImage} alt='Logo tipo' />
-      <button type='button' className='ButtonPlus'>
-        <Plus size={20} className='Plus' />
-        Novo Hábito
-      </button>
+
+      <Dialog.Root>
+        <Dialog.Trigger type='button' className='ButtonPlus'>
+          <Plus size={20} className='Plus' />
+          Novo Hábito
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className='ModalOverlay' />
+          <Dialog.Content className='ModalContent'>
+            <Dialog.Close className='ModalClose'>
+              <X size={24} className='ModalButtonClose' />
+            </Dialog.Close>
+            <Dialog.Title className='ModalTitle'>Criar Hábito</Dialog.Title>
+            <NewHabitForm />
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
